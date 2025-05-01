07b1bf42bc1fd12ac3081e660398c4a54a3f5634
Georges SARR <georgesmbissanes@gmail.com>
2025-05-01 15:05:10 +0200

    Added a processiong function that launches all the steps from raw data to vector database creation or loading.

--- embedding.py
+++ embedding.py
@@ -10,55 +10,31 @@ def create_chroma_database(
     embedding: OpenAIEmbeddings,
     override: bool = False,
     path: str = CHROMA_PATH,
-) -> bool:
+):
     if len(data) == 0:
         print("No data to save.")
-        return False
+        return
     if os.path.exists(path) and not override:
         print(f"It seems a chroma database already exists at {path}.")
-        return False
+        try:
+            return Chroma(persist_directory=path, embedding_function=embedding)
+        except Exception as e:
+            print(f"Error loading existing database: {e}")
+            print("Please delete the existing database or set override=True.")
+            return
     data_element = data[0]
     printer = lambda: print(f"Saved {len(data)} documents to {path}.")
     if isinstance(data_element, Document):
-        Chroma.from_documents(data, embedding, persist_directory=path)
+        db = Chroma.from_documents(data, embedding, persist_directory=path)
         printer()
-        return True
+        return db
     if isinstance(data_element, str):
-        Chroma.from_texts(data, embedding, persist_directory=path)
+        db = Chroma.from_texts(data, embedding, persist_directory=path)
         printer()
-        return True
-    return False
+        return db
 
 
-if __name__ == "__main__":
-    from loader import load_pdf_lazily
-
-    # from pypdf import PdfReader
-    from splitter import recursive_splitter, split_documents
-    from dotenv import load_dotenv
-
-    path = "data/s3-api.pdf"
-    ## Reading to Document
-    docs = load_pdf_lazily(path, max_nb_docs=4000, write_stdout=False)
-
-    # ## Reading to str
-    # reader = PdfReader(path)
-    # docs = [reader.pages[i].extract_text() for i in range(10)]
-
-    text_splitter = recursive_splitter(chunk_size=1000, chunk_overlap=200)
-    documents = split_documents(docs, text_splitter)
-
-    ## Loading OPENAI_API_KEY and OpenAI embeddings
-    load_dotenv()
-    embedding = OpenAIEmbeddings()
-    create_chroma_database(documents, embedding, True, CHROMA_PATH)
-
+def get_retriever(embedding: OpenAIEmbeddings, path: str = CHROMA_PATH):
     ## Reading the persisted Chroma database
-    vectordb = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding)
-    retriever = vectordb.as_retriever()
-
-    ## Retrieving most relevant answers using stored vector embeddings
-    response = retriever.invoke("What request to use to send a DELETE HTTPS request ?")
-    for doc in response:
-        print(doc.page_content)
-        print("=======================================================\n")
+    vectordb = Chroma(persist_directory=path, embedding_function=embedding)
+    return vectordb.as_retriever()
--- loader.py
+++ loader.py
@@ -1,7 +1,15 @@
+import os
+from dotenv import load_dotenv
+from langchain_chroma import Chroma
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
 from tqdm.auto import tqdm
 from pypdf import PdfReader
+
+from constants import CHROMA_PATH
+from embedding import create_chroma_database
+from splitter import recursive_splitter, split_documents
 
 
 def load_pdf(path: str):
@@ -12,9 +20,15 @@ def lazy_load_pdf(path: str):
     return PyPDFLoader(path).lazy_load()
 
 
-def load_pdf_lazily(path: str, max_nb_docs: int = 100, write_stdout: bool = False):
+def load_pdf_lazily(
+    path: str, max_nb_docs: int | None = None, write_stdout: bool = False
+):
     total_nb_pages = len(PdfReader(path).pages)
-    nb_pages = min(max_nb_docs, total_nb_pages)
+    nb_pages = (
+        min(max_nb_docs, total_nb_pages)
+        if isinstance(max_nb_docs, int)
+        else total_nb_pages
+    )
     lazy_docs = lazy_load_pdf(path)
     documents = [Document("")] * nb_pages
     for page, doc in tqdm(
@@ -31,15 +45,38 @@ def load_pdf_lazily(path: str, max_nb_docs: int = 100, write_stdout: bool = Fals
     return documents
 
 
-async def async_load_pdf(path: str):
-    return await PyPDFLoader(path).aload()
+def pre_process(
+    path: str, chunk_size: int = 1000, chunk_overlap: int = 200, override: bool = False
+):
+    if os.path.exists(CHROMA_PATH) and not override:
+        load_dotenv()
+        embedding = OpenAIEmbeddings()
+        return Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding)
+    docs = load_pdf_lazily(
+        path
+    )  # TODO use here a load function that loads any common document format (.pdf, .docx, .md, etc)
 
+    ## Split the data
+    text_splitter = recursive_splitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    )
+    documents = split_documents(docs, text_splitter)
 
-async def async_lazy_load_pdf(path: str):
-    return await PyPDFLoader(path).alazy_load()
+    ## Loading OPENAI_API_KEY and OpenAI embeddings
+    load_dotenv()
+    embedding = OpenAIEmbeddings()
+    return create_chroma_database(documents, embedding, override, CHROMA_PATH)
 
 
 if __name__ == "__main__":
     path = "data/s3-api.pdf"
-    docs = lazy_load_pdf(path)
-    print(next(docs).page_content)
+    # docs = lazy_load_pdf(path)
+    # print(next(docs).page_content)
+
+    vector_db = pre_process(
+        path=path, chunk_size=1000, chunk_overlap=200, override=False
+    )
+    response = vector_db.as_retriever().invoke("Which langauages are supported by s3?")
+    for doc in response:
+        print(doc.page_content)
+        print("=======================================================\n")
