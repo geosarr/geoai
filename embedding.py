fca064f9ede17a673bcebfb671b0ab7a8b4ef273
Georges SARR <georgesmbissanes@gmail.com>
2025-05-01 09:27:48 +0200

    Added texts as input of chroma db creation.

--- embedding.py
+++ embedding.py
@@ -6,29 +6,44 @@ from langchain_core.documents import Document
 
 
 def create_chroma_database(
-    documents: list[Document],
+    data: list[Document] | list[str],
     embedding: OpenAIEmbeddings,
     override: bool = False,
+    path: str = CHROMA_PATH,
 ) -> bool:
-    if os.path.exists(CHROMA_PATH) and not override:
-        print(f"A chroma database already exists at {CHROMA_PATH}.")
+    if len(data) == 0:
+        print("No data to save.")
         return False
-    Chroma.from_documents(documents, embedding, persist_directory=CHROMA_PATH)
-    print(f"Saved {len(documents)} documents to {CHROMA_PATH}.")
+    if os.path.exists(path) and not override:
+        print(f"It seems a chroma database already exists at {path}.")
+        return False
+    data_element = data[0]
+    printer = lambda: print(f"Saved {len(data)} documents to {path}.")
+    if isinstance(data_element, Document):
+        Chroma.from_documents(data, embedding, persist_directory=path)
+        printer()
+    if isinstance(data_element, str):
+        Chroma.from_texts(data, embedding, persist_directory=path)
+        printer()
     return True
 
 
 if __name__ == "__main__":
-    from loader import load_pdf_lazily
+    # from loader import load_pdf_lazily
+    # from pypdf import PdfReader
     from dotenv import load_dotenv
 
-    path = "data/s3-api.pdf"
-    documents = load_pdf_lazily(path, max_nb_docs=3497, write_stdout=False)
+    # path = "data/s3-api.pdf"
+    # # Reading to Document
+    # documents = load_pdf_lazily(path, max_nb_docs=4000, write_stdout=False)
+
+    # # Reading to str
+    # reader = PdfReader(path)
+    # documents = [reader.pages[i].extract_text() for i in range(10)]
 
     load_dotenv()
     embedding = OpenAIEmbeddings()
-    if create_chroma_database(documents, embedding, True):
-        print("Chroma database created.")
+    # create_chroma_database(documents, embedding, False, CHROMA_PATH)
 
     vectordb = Chroma(persist_directory=CHROMA_PATH, embedding_function=embedding)
     retriever = vectordb.as_retriever()
